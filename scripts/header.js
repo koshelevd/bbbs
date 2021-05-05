a80d394f030f47209fee50fecb08f9e96053131d
Denis Lozhkin <ssdenis00@yandex.ru>
2021-05-05 16:56:22 +0300

    feat: search mobile

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,9 @@
-const headerMenu = document.querySelector('.header__menu');
+const headerMenu = document.querySelector('.header__menu-mobile');
 const headerBtnBurger = document.querySelector('.header__burger-btn');
 const headerAuthorization = document.querySelector('.header__authorization');
 const headerBtnClose = document.querySelector('.header__close-btn');
+const searchBtn = document.querySelector('.header__loupe');
+const searchContainer = document.querySelector('.header__search');
 
 function openMenu() {
   headerMenu.classList.add('header__menu_visible_on');
@@ -17,5 +19,12 @@ function closeMenu() {
   headerBtnClose.classList.remove('header__close-btn_visible_on');
 }
 
+function handleSearchBtn() {
+  searchContainer.classList.toggle('header__search_visible_on');
+}
+
 headerBtnBurger.addEventListener('click', openMenu);
+
 headerBtnClose.addEventListener('click', closeMenu);
+
+searchBtn.addEventListener('click', handleSearchBtn);
