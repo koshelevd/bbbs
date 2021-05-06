57a7c7f280c25ad5221f29c95ce919baddeb47d4
Denis Lozhkin <ssdenis00@yandex.ru>
2021-05-06 12:49:53 +0300

    feat: popup__authorization

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,7 @@
 const headerMenu = document.querySelector('.header__menu-mobile');
 const headerBtnBurger = document.querySelector('.header__burger-btn');
-const headerAuthorization = document.querySelector('.header__authorization');
+const headerAuthorizationMobile = document.querySelector('.header__authorization_screen_mobile');
+const headerAuthorizationDesktop = document.querySelector('.header__authorization_screen_desktop');
 const headerBtnClose = document.querySelector('.header__close-btn');
 const searchBtnMobile = document.querySelector('.header__loupe_screen_mobile');
 const searchContainerMobile = document.querySelector('.search_screen_mobile');
@@ -8,17 +9,19 @@ const searchBtnDesktop = document.querySelector('.header__loupe_screen_desktop')
 const searchContainerDesktop = document.querySelector('.search_screen_desktop');
 const searchBlockLoupe = document.querySelector('.header__search-authorization');
 const menuDesktop = document.querySelector('.header__menu-desktop');
+const popupAuthorization = document.querySelector('.popup_type_authorization');
+const popupAuthorizationClose = popupAuthorization.querySelector('.popup__close');
 
 function openMenu() {
   headerMenu.classList.add('header__menu_visible_on');
-  headerAuthorization.classList.add('header__authorization_visible_on');
+  headerAuthorizationMobile.classList.add('header__authorization_visible_on');
   headerBtnBurger.classList.add('header__burger-btn_visible_off');
   headerBtnClose.classList.add('header__close-btn_visible_on');
 }
 
 function closeMenu() {
   headerMenu.classList.remove('header__menu_visible_on');
-  headerAuthorization.classList.remove('header__authorization_visible_on');
+  headerAuthorizationMobile.classList.remove('header__authorization_visible_on');
   headerBtnBurger.classList.remove('header__burger-btn_visible_off');
   headerBtnClose.classList.remove('header__close-btn_visible_on');
 }
@@ -33,6 +36,14 @@ function handleSearchBtnDesktop() {
   searchBlockLoupe.classList.toggle('header__search-authorization_container_opened');
 }
 
+function openPopup() {
+  popupAuthorization.classList.add('popup_visible_on');
+}
+
+function closePopup() {
+  popupAuthorization.classList.remove('popup_visible_on');
+}
+
 headerBtnBurger.addEventListener('click', openMenu);
 
 headerBtnClose.addEventListener('click', closeMenu);
@@ -40,3 +51,9 @@ headerBtnClose.addEventListener('click', closeMenu);
 searchBtnMobile.addEventListener('click', handleSearchBtnMobile);
 
 searchBtnDesktop.addEventListener('click', handleSearchBtnDesktop);
+
+headerAuthorizationMobile.addEventListener('click', openPopup);
+
+headerAuthorizationDesktop.addEventListener('click', openPopup);
+
+popupAuthorizationClose.addEventListener('click', closePopup);
